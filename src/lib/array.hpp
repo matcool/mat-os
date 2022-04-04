f679bfed0bb4f9eb2dbdc3b873f2b398f508559a
matcool <26722564+matcool@users.noreply.github.com>
2022-04-03 23:48:47 -0300

    View

--- src/lib/array.hpp
+++ src/lib/array.hpp
@@ -26,3 +26,19 @@ public:
 	T& operator[](const size_t i) { return m_array[i]; }
 	T& at(const size_t i) { return m_array[i]; }
 };
+
+// a non owning view into an array of T
+template <class T>
+class View : public Iterable<View<T>> {
+	T* m_data;
+	size_t m_size;
+public:
+	View(T* const data, const size_t size) : m_data(data), m_size(size) {}
+
+	size_t size() const { return m_size; }
+	const T* data() const { return m_data; }
+	T* data() { return m_data; }
+
+	T& operator[](const size_t i) { return m_data[i]; }
+	const T& operator[](const size_t i) const { return m_data[i]; }
+};
