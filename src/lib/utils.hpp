83d140777e9334b081325efa372697be952ff86a
matcool <26722564+matcool@users.noreply.github.com>
2022-04-03 23:52:33 -0300

    copy and swap
    maybe make an algorithms header in the future and move copy there

--- src/lib/utils.hpp
+++ src/lib/utils.hpp
@@ -6,3 +6,27 @@ struct Pair {
 	A first;
 	B second;
 };
+
+template <class A, class B>
+bool operator==(const Pair<A, B>& a, const Pair<A, B>& b) {
+	return a.first == b.first && a.second == b.second;
+}
+
+template <class Input, class Output>
+void copy(Input begin, Input end, Output out) {
+	for (; begin != end; ++begin, ++out)
+		*out = *begin;
+}
+
+template <class T>
+requires requires (T& a) { a.swap(a); }
+void swap(T& a, T& b) {
+	a.swap(b);
+}
+
+template <class T>
+void swap(T& a, T& b) {
+	const T copy = a;
+	a = b;
+	b = copy;
+}
