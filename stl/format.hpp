c1855022cbb095c29e87659994c2e30ffca39318
matcool <26722564+matcool@users.noreply.github.com>
2023-07-06 21:56:17 -0300

    add some comments on formatting code

--- stl/format.hpp
+++ stl/format.hpp
@@ -61,19 +61,32 @@ struct Formatter<Func, StringView> {
 	}
 };
 
+// Formats a given format string into an output function,
+// which accepts each character at a time.
+// The format string follows the python-like {} formatting,
+// where {} is the placeholder for a value of any (formattable) type.
 template <FormatOutFunc Func, class... Args>
 void format_to(Func func, StringView str, const Args&... args) {
+	// no formatting arguments provided, just return the whole string
 	if constexpr (sizeof...(Args) == 0) {
 		for (char c : str) {
 			func(c);
 		}
 	} else {
+		// the index for which placeholder we are currently on
 		usize index = 0;
+
+		// much of this indirection is used to avoid doing any allocations,
+		// while still being able to "index" the list of arguments based on a
+		// runtime index
 		
+		// array of type-erased args, for the runtime part of the code
 		const void* const arg_ptrs[] = { reinterpret_cast<const void*>(&args)... };
 		
+		// array of function pointers, capable of formatting each argument
+		// takes in a void* as to be type-erased
 		using InnerFunc = void(*)(Func&, const void*, StringView);
-		InnerFunc funcs[] = {
+		InnerFunc arg_funcs[] = {
 			+[](Func& func, const void* arg, StringView spec) {
 				using Fmter = Formatter<Func, types::decay<Args>>;
 				if constexpr (requires(Func& func, Args value, StringView str) { Fmter::format(func, value, str); }) {
@@ -86,17 +99,26 @@ void format_to(Func func, StringView str, const Args&... args) {
 		
 		for (usize i = 0; i < str.size(); ++i) {
 			const char cur = str[i];
+			// if we're at the end of the string then assume
+			// this isnt the start of a placeholder
 			if (i == str.size() - 1) {
 				func(cur);
 			} else {
 				const char next = str[i + 1];
 				if (cur == '{' || cur == '}') {
 					if (next == cur) {
+						// escaping the characters used for the placeholders
+						// is done by duplicating them, so if the current character
+						// and next character are the same, skip the next one.
 						++i;
 						func(cur);
 					} else if (cur == '{') {
 						auto close_index = str.slice(i).find('}');
-						if (close_index == usize(-1)) return;
+						if (close_index == usize(-1)) {
+							// no closing bracket found, so error
+							// TODO: error
+							return;
+						}
 
 						close_index += i;
 						const auto current_placeholder = str.slice(i, close_index);
@@ -108,13 +130,15 @@ void format_to(Func func, StringView str, const Args&... args) {
 						}
 
 						if (index >= sizeof...(Args)) {
-							// error
+							// we've gone through more placeholders then there are args, so error
+							// TODO: error
 							return;
 						}
-						funcs[index](func, arg_ptrs[index], specifiers);
+						arg_funcs[index](func, arg_ptrs[index], specifiers);
 						++index;
 					} else {
-						// error
+						// we hit a } which wasnt actually started, so error
+						// TODO: error
 						return;
 					}
 				} else {
