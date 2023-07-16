ad8787b95a9fe7ac3dfaa87a9a958b7c61c106c4
matcool <26722564+matcool@users.noreply.github.com>
2023-07-16 19:37:45 -0300

    add simple bit_mask function

--- stl/math.hpp
+++ stl/math.hpp
@@ -11,4 +11,10 @@ Int div_ceil(Int a, Int b) {
 	return a / b + !!(a % b);
 }
 
+// Returns an integer with the first "n" bits set to 1.
+template <concepts::integral Int>
+constexpr Int bit_mask(Int n) {
+	return (1 << n) - 1;
+}
+
 }
