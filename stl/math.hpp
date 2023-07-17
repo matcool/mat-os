edf90a01627515db1b65504d400e9959f1951c24
matcool <26722564+matcool@users.noreply.github.com>
2023-07-16 21:52:12 -0300

    set_bit

--- stl/math.hpp
+++ stl/math.hpp
@@ -7,7 +7,7 @@ namespace STL_NS::math {
 
 // Divides two integers and return the ceiling
 template <concepts::integral Int>
-Int div_ceil(Int a, Int b) {
+constexpr Int div_ceil(Int a, Int b) {
 	return a / b + !!(a % b);
 }
 
@@ -15,6 +15,13 @@ Int div_ceil(Int a, Int b) {
 template <concepts::integral Int>
 constexpr Int bit_mask(Int n) {
 	return (1 << n) - 1;
+}
+
+// Sets a specific bit at "idx" to a value.
+template <concepts::integral Int>
+constexpr void set_bit(Int& target, u64 idx, bool value) {
+	const Int mask = 1 << idx;
+	target = (target & ~mask) | (value << idx);
 }
 
 }
