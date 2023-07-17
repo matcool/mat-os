47a12728ebd2851e4e95ed4b8cfac43d66f53f36
matcool <26722564+matcool@users.noreply.github.com>
2023-07-17 01:11:48 -0300

    fix bitwise operations

--- stl/math.hpp
+++ stl/math.hpp
@@ -14,13 +14,13 @@ constexpr Int div_ceil(Int a, Int b) {
 // Returns an integer with the first "n" bits set to 1.
 template <concepts::integral Int>
 constexpr Int bit_mask(Int n) {
-	return (1 << n) - 1;
+	return (Int(1) << n) - 1;
 }
 
 // Sets a specific bit at "idx" to a value.
 template <concepts::integral Int>
 constexpr void set_bit(Int& target, u64 idx, bool value) {
-	const Int mask = 1 << idx;
+	const Int mask = Int(1) << idx;
 	target = (target & ~mask) | (value << idx);
 }
 
