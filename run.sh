b5610460f7b20177a6f85cc481c6551c3fd92003
matcool <26722564+matcool@users.noreply.github.com>
2023-07-18 23:44:02 -0300

    add wsl option for mat

--- run.sh
+++ run.sh
@@ -3,8 +3,15 @@
 NAME=kernel.iso
 BUILT_PATH=build/$NAME
 
+QEMU=qemu-system-x86_64
+
 if [ "$1" == "debug" ]; then
 	EXTRA_ARGS="-s -S"
 fi
+if [ "$1" == "wsl" ]; then
+	QEMU=qemu-system-x86_64w
+	BUILT_PATH=$(wslpath -w "$BUILT_PATH")
+	EXTRA_ARGS="-display sdl"
+fi
 
-qemu-system-x86_64 -M q35 -m 1G -cdrom $BUILT_PATH -boot d -serial stdio $EXTRA_ARGS
+$QEMU -M q35 -m 1G -cdrom $BUILT_PATH -boot d -serial stdio $EXTRA_ARGS
