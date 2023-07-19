dd1381415c7cae6492dbec10ff6bcc2ffa2c978f
matcool <26722564+matcool@users.noreply.github.com>
2023-07-18 23:35:38 -0300

    support static initializers

--- kernel/cxa.cpp
+++ kernel/cxa.cpp
@@ -1,61 +1,16 @@
-#include <stdint.h>
-#include <stddef.h>
-
-// GCC and Clang reserve the right to generate calls to the following
-// 4 functions even if they are not directly called.
-// Implement them as the C specification mandates.
-// DO NOT remove or rename these functions, or stuff will eventually break!
-// They CAN be moved to a different .c file.
+#include <stl/types.hpp>
 
 extern "C" {
-	void *memcpy(void *dest, const void *src, size_t n) {
-		uint8_t *pdest = (uint8_t *)dest;
-		const uint8_t *psrc = (const uint8_t *)src;
+	// https://libcxxabi.llvm.org/spec.html
 
-		for (size_t i = 0; i < n; i++) {
-			pdest[i] = psrc[i];
-		}
-
-		return dest;
+	int __cxa_guard_acquire(u64* guard) {
+		// Effects: This function is called before initialization takes place.
+		// Returns: 1 if the initialization is not yet complete, otherwise 0.
+		return *reinterpret_cast<u8*>(guard) == 0;
 	}
 
-	void *memset(void *s, int c, size_t n) {
-		uint8_t *p = (uint8_t *)s;
-
-		for (size_t i = 0; i < n; i++) {
-			p[i] = (uint8_t)c;
-		}
-
-		return s;
-	}
-
-	void *memmove(void *dest, const void *src, size_t n) {
-		uint8_t *pdest = (uint8_t *)dest;
-		const uint8_t *psrc = (const uint8_t *)src;
-
-		if (src > dest) {
-			for (size_t i = 0; i < n; i++) {
-				pdest[i] = psrc[i];
-			}
-		} else if (src < dest) {
-			for (size_t i = n; i > 0; i--) {
-				pdest[i-1] = psrc[i-1];
-			}
-		}
-
-		return dest;
-	}
-
-	int memcmp(const void *s1, const void *s2, size_t n) {
-		const uint8_t *p1 = (const uint8_t *)s1;
-		const uint8_t *p2 = (const uint8_t *)s2;
-
-		for (size_t i = 0; i < n; i++) {
-			if (p1[i] != p2[i]) {
-				return p1[i] < p2[i] ? -1 : 1;
-			}
-		}
-
-		return 0;
+	void __cxa_guard_release(u64* guard) {
+		// Effects: Sets the first byte of the guard object to a non-zero value.
+		*reinterpret_cast<u8*>(guard) = 0x44;
 	}
 }
