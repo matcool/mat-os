bc4cc43adf48102abaf5a047fc82d9fed627af6d
matcool <26722564+matcool@users.noreply.github.com>
2023-07-22 12:18:03 -0300

    kernel: mark allocation functions as nodiscard

--- kernel/memory/allocator.hpp
+++ kernel/memory/allocator.hpp
@@ -10,7 +10,7 @@ namespace alloc {
 void init_physical_allocator();
 
 // "Allocates" a physical page of memory by marking it as used.
-PhysicalAddress allocate_physical_page();
+[[nodiscard]] PhysicalAddress allocate_physical_page();
 
 // Frees a physical page of memory by marking it as unused.
 void free_physical_page(PhysicalAddress addr);
@@ -20,11 +20,11 @@ void init_virtual_allocator();
 
 // Allocates a page of virtual memory. Equivalent to `allocate_pages(1)`.
 // The page will be mapped as RWX.
-void* allocate_page();
+[[nodiscard]] void* allocate_page();
 
 // Allocates "count" continuous pages.
 // Each one should be freed individually (i think)
-void* allocate_pages(usize count);
+[[nodiscard]] void* allocate_pages(usize count);
 
 // Frees a single virtual page that was allocated via allocate_page(s).
 void free_page(void* addr);
@@ -35,7 +35,7 @@ void init_heap_allocator();
 // Allocates some amount of memory on the kernel heap.
 // Returned pointer will be aligned to 16 bytes.
 // In case of any failures, nullptr will be returned.
-void* heap_allocate(usize bytes);
+[[nodiscard]] void* heap_allocate(usize bytes);
 
 // Frees some memory allocated by heap_allocate.
 void heap_free(void* ptr);
