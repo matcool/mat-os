2e138d77e512c400b024d73a43221663e4f22d76
matcool <26722564+matcool@users.noreply.github.com>
2023-07-23 01:27:04 -0300

    kernel: calculate clipping rects for windows

--- kernel/screen/windows.cpp
+++ kernel/screen/windows.cpp
@@ -10,6 +10,39 @@
 using Point = math::Vec2<i32>;
 using Rect = math::Rect<i32>;
 
+// Cuts out `cut` from `target`, resulting in at most 4 smaller rects.
+Vector<Rect> split_rect(Rect target, const Rect& cut) {
+	Vector<Rect> out(4);
+
+	// split by left edge
+	if (cut.left() >= target.left() && cut.left() <= target.right()) {
+		out.push(Rect::from_corners(target.pos, Point(cut.left() - 1, target.bottom())));
+		target.size.width -= cut.left() - target.left();
+		target.pos.x = cut.pos.x;
+	}
+
+	// split by top edge
+	if (cut.top() >= target.top() && cut.top() <= target.bottom()) {
+		out.push(Rect::from_corners(target.pos, Point(target.right(), cut.top() - 1)));
+		target.size.height -= cut.top() - target.top();
+		target.pos.y = cut.pos.y;
+	}
+
+	// split by right edge
+	if (cut.right() >= target.left() && cut.right() <= target.right()) {
+		out.push(Rect::from_corners(Point(cut.right() + 1, target.top()), target.bot_right()));
+		target.size.width -= target.right() - cut.right();
+	}
+
+	// split by bottom edge
+	if (cut.bottom() >= target.top() && cut.bottom() <= target.bottom()) {
+		out.push(Rect::from_corners(Point(target.left(), cut.bottom() + 1), target.bot_right()));
+		target.size.height -= target.bottom() - cut.bottom();
+	}
+
+	return out;
+}
+
 struct Window {
 	Rect rect;
 	Color fill_color;
@@ -31,15 +64,27 @@ struct Desktop {
 	bool last_pressed = false;
 	Window* dragged_window = nullptr;
 	Point drag_offset = Point(0, 0);
+
+	Vector<Rect> clip_rects;
 	
 	u64 last_render = 0;
 
 	void paint() {
+		clip_rects.clear();
+
 		// draw background
 		context->fill(0, 0, context->width(), context->height(), 0xFF3399FF);
 
 		for (auto& win : children) {
 			win.paint(context);
+			add_clip_rect(win.rect);
+		}
+
+		for (auto& rect : clip_rects) {
+			context->fill(rect.pos.x, rect.pos.y, rect.size.width, 1, Color(255, 255, 0));
+			context->fill(rect.pos.x, rect.pos.y, 1, rect.size.height, Color(255, 255, 0));
+			context->fill(rect.right(), rect.pos.y, 1, rect.size.height, Color(200, 200, 0));
+			context->fill(rect.pos.x, rect.bottom(), rect.size.width, 1, Color(200, 200, 0));
 		}
 
 		context->fill(mouse_pos.x, mouse_pos.y, 10, 10, Color(0, 0, 0));
@@ -84,6 +129,24 @@ struct Desktop {
 			paint();
 			last_render = kernel::pit::get_ticks();
 		}
+	}
+
+	void add_clip_rect(const Rect& rect) {
+		for (usize i = 0; i < clip_rects.size(); ) {
+			if (!clip_rects[i].intersects(rect)) {
+				++i;
+				continue;
+			}
+
+			// new rectangle intersects existing one, needs to be split
+			const auto target = clip_rects[i];
+			clip_rects.remove(i);
+			clip_rects.concat(split_rect(target, rect).span());
+
+			// reiterate because of the new rects..
+			i = 0;
+		}
+		clip_rects.push(rect);
 	}
 };
 
