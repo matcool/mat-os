050f53ac8196e4f5e6872dfbdbeb9ba4e7be5960
matcool <26722564+matcool@users.noreply.github.com>
2023-07-30 21:47:17 -0300

    meta: demangle c++ symbols in more places in gdb

--- debug.sh
+++ debug.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ $# -eq 1 ]; then
-	gdb -ex "target remote :1234" -ex "set disassembly-flavor intel" -ex "b $1" -ex "continue" build/kernel/kernel
+	gdb -ex "target remote :1234" -ex "set disassembly-flavor intel" -ex "set print asm-demangle on" -ex "b $1" -ex "continue" build/kernel/kernel
 else
-	gdb -ex "target remote :1234" -ex "set disassembly-flavor intel" build/kernel/kernel
+	gdb -ex "target remote :1234" -ex "set disassembly-flavor intel" -ex "set print asm-demangle on" build/kernel/kernel
 fi
